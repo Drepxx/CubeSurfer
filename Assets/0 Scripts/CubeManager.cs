51b5baa7c0ddbc1ba3490ab42cd2ea8a4a506e1b
Drepxx <erayats16@gmail.com>
2021-12-15 01:47:31 +0300

    FallSmooth

--- Assets/0 Scripts/CubeManager.cs
+++ Assets/0 Scripts/CubeManager.cs
@@ -8,44 +8,42 @@ public class CubeManager : MonoBehaviour
     public Texture[] texture = new Texture[3];
     public List<GameObject> cubes = new();
     public static CubeManager instance;
-   // public GameObject SpawnCube;
+    // public GameObject SpawnCube;
     public GameObject playerParent;
-
+    public int totalCount = 0;
 
     public void Awake()
     {
         instance = this;
-
     }
 
     private void Update()
     {
         if (isObjectExit)
         {
-           // Player.instance.transform.DOMoveY(-count, 0.2f).SetRelative();
-            
+            for (int i = playerParent.transform.childCount - 1; i >= 0; i--)
+            {
+                playerParent.transform.GetChild(i).transform.DOMoveY(-count, 0.1f * (playerParent.transform.childCount - i)).SetRelative();
+            }
+            totalCount += count;
             Player.instance.horizontalSpeed = 1;
             isObjectExit = false;
+            Debug.Log(playerParent.transform.childCount);
             count = 0;
         }
     }
     public void Color(GameObject textureCube)
     {
-        
         int rnd = Random.Range(0, 3);
-        textureCube.GetComponent<MeshRenderer>().material.mainTexture=texture[rnd];
-
-
+        textureCube.GetComponent<MeshRenderer>().material.mainTexture = texture[rnd];
     }
     public void Add(GameObject collectedCube)
     {
-
         collectedCube.transform.tag = ("Player");
         cubes.Add(collectedCube);
-        collectedCube.transform.SetParent(Player.instance.transform);
-        collectedCube.transform.position = new Vector3(Player.instance.transform.position.x, Player.instance.transform.position.y - cubes.Count, Player.instance.transform.position.z);
-        Player.instance.transform.DOMoveY(Player.instance.transform.position.y + 1, 0.2f);
-
+        collectedCube.transform.SetParent(playerParent.transform);
+        collectedCube.transform.position = new Vector3(Player.instance.transform.position.x, Player.instance.transform.position.y - cubes.Count - totalCount, Player.instance.transform.position.z);
+        playerParent.transform.DOMoveY(playerParent.transform.position.y + 1, 0.2f);
     }
     public int count = 0;
     public bool isObjectExit;
